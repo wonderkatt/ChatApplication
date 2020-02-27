0c088951929e57696a146c190bb23ffa6888dd44
wonderkatt <55135528+wonderkatt@users.noreply.github.com>
2020-02-27 14:52:46 +0100

    Chatapplication course, parts 1,2,3 and 4.

---
+++ UDPChatApp/UDPChatApp/UDPChatServer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPChatApp
+{
+    public class UDPChatServer
+    {
+        Socket mSockBroadcastReciever;
+        IPEndPoint mIPEPLocal;
+        private int retryCount;
+
+        public UDPChatServer()
+        {
+            mSockBroadcastReciever = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            mIPEPLocal = new IPEndPoint(IPAddress.Any, 23000);
+
+            mSockBroadcastReciever.EnableBroadcast = true;
+        }
+
+        public void StartRecievingData()
+        {
+            try
+            {
+                SocketAsyncEventArgs socketAEA = new SocketAsyncEventArgs();
+                socketAEA.SetBuffer(new byte[1024], 0, 1024);
+                socketAEA.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                if(!mSockBroadcastReciever.IsBound)
+                {
+                    mSockBroadcastReciever.Bind(mIPEPLocal);
+                }
+                socketAEA.Completed += RecieveCompletedCallback;
+
+                if(!mSockBroadcastReciever.ReceiveFromAsync(socketAEA))
+                {
+                    Console.WriteLine($"Failed to reciev data - socket error: {socketAEA.SocketError}");
+                    if(retryCount++ >= 10)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        StartRecievingData();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+        private void RecieveCompletedCallback(object sender, SocketAsyncEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
